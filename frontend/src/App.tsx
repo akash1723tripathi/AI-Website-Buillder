d0fa572a289c3178532b63192667f3278d4b08a0
akash1723tripathi <akashtripathi.acad@gmail.com>
2026-01-27 00:26:37 +0530

    Frontend Setup Completed

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Projects from "./pages/Projects"
+import Community from "./pages/Community"
+import Pricing from "./pages/Pricing"
+import Preview from "./pages/Preview"
+import MyProjects from "./pages/MyProjects"
+
+function App() {
+
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects/:projectId" element={<Projects />} />
+        <Route path="/projects" element={<MyProjects />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/preview" element={<Preview />} />
+        <Route path="/community" element={<Community />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
